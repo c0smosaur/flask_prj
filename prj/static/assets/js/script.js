ffc7df2a9680ae85f2f7016729713198afb83a2c
cosmosaur <119040660+c0smosaur@users.noreply.github.com>
2023-01-19 15:39:56 +0900

    01.19.2023
    corrected minor js errors

--- prj/static/assets/js/script.js
+++ prj/static/assets/js/script.js
@@ -2,15 +2,15 @@ const check = document.getElementsByName("pre_toggle_2")[0];
 const width = document.getElementsByName("width")[0];
 const height = document.getElementsByName("height")[0];
 
-function f () {
+function handleCheck() {
     console.log(check.checked);
     if (check.checked){
         width.style.display='inline';
-        height.style.display='inline'
+        height.style.display='inline';
     } else {
         width.style.display='none';
-        height.style.display='none'
+        height.style.display='none';
     }
 }
 
-check.addEventListener("click", f)
+check.addEventListener("click", handleCheck);
